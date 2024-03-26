ef05297c5270fb612049021bfee22d39aec376e8
Rajarshi Roy <roystark.dev@gmail.com>
2024-03-26 10:23:22 +0530

    Tweaking around

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 from app import app  # 1st app is filename, 2nd app is variable name
 
+
 load_dotenv()
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
--- routes.py
+++ routes.py
@@ -6,9 +6,11 @@ from app import app
 def index():
     return render_template("index.html")
 
+
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    return render_template('login.html')
+
 
 @app.route("/register")
 def register():
